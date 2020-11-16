922e3ddadd16aaf3fd893d4a93344964bca158e8
Muskan1411 <muskan99ag@gmail.com>
2020-11-16 20:03:56 +0530

    accessing context - part 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-    render() { 
-        return (
-			<nav>
+	static contextType = ThemeContext;
+	render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+		return (
+			<nav style={{ background: theme.ui, color: theme.syntax }}>
 				<h1>Context App</h1>
 				<ul>
 					<li>Home</li>
@@ -12,7 +16,7 @@ class Navbar extends Component {
 				</ul>
 			</nav>
 		);
-    }
+	}
 }
- 
+
 export default Navbar;
